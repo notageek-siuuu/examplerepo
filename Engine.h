d42e5fdd877794a81b656e9919804a2f9a8f9c91
notageek-siuuu <165657881+notageek-siuuu@users.noreply.github.com>
2024-04-24 22:51:11 +0400

    Update Engine.h

--- Engine.h
+++ Engine.h
@@ -3,8 +3,9 @@
 
 #include "mbed.h"
 #include "N5110.h"
-#include "Joystick.h"
+
 #include "Ball.h"
+
 #include "Zero.h"
 #include "One.h"
 #include "Two.h"
@@ -15,18 +16,85 @@
 
 // gap from edge of screen
 
-class PongEngine {
-    public:
-        PongEngine();  // pass in the lcd object from the main file
-        void init(int paddle_position,int paddle_height,int paddle_width,int ball_size,int speed);
-        int update(UserInput input);
-        void draw(N5110 &lcd);
-    private:
-        void check_wall_collision();
-        void check_paddle_collision();
-        void check_goal();
-        Ball _ball;
-        int _lives;
+class ImpossEngine
+{
+
+public:
+
+    /** Constructor */
+    ImpossEngine();
+    
+    /** Destructor */
+    ~ImpossEngine();
+
+   
+    int update(UserInput input);
+    /** Updates all of the values for the ball and maps
+         * @param pad Gamepad object
+         * @param lcd N5110 object
+         */
+    void update(N5110 &lcd);
+    
+    /** Draws the level, ball and objects in the levels
+         * @param pad Gamepad object
+         * @param lcd N5110 object
+         */
+    void draw(N5110 &lcd);
+    
+    /** Goes to the start menu, also initialises the ball
+         * @param pad Gamepad object
+         * @param lcd N5110 object
+         */
+    void complete(N5110 &lcd);
+    
+    /** Sets the level to zero
+         */
+    void set_level_zero();
+    
+    /** Stores the value of the level
+         */
+    int level;
+
+    
+    void level_finish();
+    
+private:
+
+    /** Checks for a collision between the ball and a wall/object
+         * @param pad Gamepad object
+         * @param lcd N5110 object
+         */
+    void check_collision( N5110 &lcd);
+    
+    
+    
+    Ball _ball; //Ball function call
+    One _one; //Level Zero function call
+    Zero _zero; //Level One function call
+    Two _two; // Level Two function call
+    Three _three; // Level Three function call
+    Four _four; // Level Four function call
+    Five _five; // Level Five function call
+    
+    /* Stores the x position of the ball */
+    int x_pos;
+    
+    /* Stores the y postition of the ball */
+    int y_pos;
+    
+    /* Used as a counter to check if a collision has occured */
+    int x;
+    
+    /* Used as a counter to check if a collision has occured */
+    int y;
+    
+    /* Used to store the value returned when fetching the x co-ordinate of the
+    balle */
+    int pos;
+    
+    Direction _d; //Direction function call
+    float _mag; //Magnitude function call
+
 };
 
 #endif
